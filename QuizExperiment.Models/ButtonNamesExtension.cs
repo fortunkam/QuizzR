bb2edc0bb114b6770c928c7812321a6e2905203c
Matthew Fortunka <matthew@fortunka.co.uk>
2023-01-02 14:01:44 +0000

    game styling changes complete

---
+++ QuizExperiment.Models/ButtonNamesExtension.cs
@@ -0,0 +1,18 @@
+﻿
+namespace QuizExperiment.Models
+{
+    public static class ButtonNamesExtension
+    {
+        public static string GetButtonNameFromIndex(this int index)
+        {
+            return index switch
+            {
+                0 => "Y",
+                1 => "X",
+                2 => "B",
+                3 => "A",
+                _ => "_"
+            };
+        }
+    }
+}
